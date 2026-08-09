8c57b23d7187a0842f9a06a5d53a1e34427bd7cb
Ioana <ioana3popescu@gmail.com>
2026-08-09 16:58:38 +0200

    feat: implement savings account logic

---
+++ src/main/java/com/factoriaf5/SavingsAccount.java
@@ -0,0 +1,61 @@
+package com.factoriaf5;
+
+public class SavingsAccount extends Account {
+
+    protected boolean isActive;
+
+      public SavingsAccount (float balance, float annualInterestRate) {
+       super(balance, annualInterestRate);
+
+       this.updateStatus();
+      }
+
+    protected void updateStatus() {
+        if (this.balance >= 10000) {
+            this.isActive = true;
+        } else {
+            this.isActive = false;
+
+        }
+    }
+     @Override
+    protected void deposit(float amount) {
+        if (this.isActive){
+        super.deposit(amount);
+        this.updateStatus();
+        }
+    }
+      
+    @Override
+    protected void withdraw(float amount) {
+        if (this.isActive){
+        super.withdraw(amount);
+        this.updateStatus();
+        }
+    }
+ 
+
+    @Override
+    protected void calculateMonthlyStatement(){      
+      if (this.numberOfWithdrawals > 4){
+        int extraWithdrawals = this.numberOfWithdrawals - 4;
+        this.monthlyFee =this.monthlyFee + (extraWithdrawals * 1000);
+      } 
+        super.calculateMonthlyStatement();
+        this.updateStatus();
+      }
+      
+      @Override
+      protected void print (){
+     System.out.println("Savings account balance:" + this.balance);
+     System.out.println("Savings account monthly fee:" + this.monthlyFee);
+     System.out.println("Savings account total transactions:" + (this.numberOfDeposits + this.numberOfWithdrawals));
+      }
+
+     }
+
+    
+
+  
+
+
