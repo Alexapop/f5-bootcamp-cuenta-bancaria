94555a1335e7131a914812ee9648ceeb5eda0881
Ioana <ioana3popescu@gmail.com>
2026-08-10 15:53:07 +0200

    feat:implement logic for current account

---
+++ src/main/java/com/factoriaf5/CurrentAccount.java
@@ -0,0 +1,51 @@
+package com.factoriaf5;
+
+public class CurrentAccount extends Account {
+
+    protected float overdraft = 0f;
+
+    public CurrentAccount(float balance, float annualInterestRate) {
+        super(balance, annualInterestRate);
+    }
+
+    @Override
+    protected void withdraw(float amount) {
+
+        if (amount <= balance) {
+            super.withdraw(amount);
+        } else {
+            this.overdraft = this.overdraft + (amount - this.balance);
+            this.balance = 0;
+            this.numberOfWithdrawals++;
+        }
+
+    }
+
+    @Override
+    protected void deposit(float amount) {
+        super.deposit(amount);
+        if (overdraft <= amount) {
+        this.balance = this.balance - this.overdraft;
+        this.overdraft = 0;
+
+        } else {
+            this.overdraft = this.overdraft -amount;
+            this.balance = 0;
+        }
+    }
+    
+
+    @Override
+    protected void calculateMonthlyStatement(){
+        super.calculateMonthlyStatement();
+    }
+ 
+     @Override
+     protected void print (){
+        System.out.println("Balance:" + this.balance);
+        System.out.println("Monthly interest:" + this.monthlyFee);
+        System.out.println("Total number of transanctions:" + (this.numberOfWithdrawals + this.numberOfDeposits));
+        System.out.println("Overdraft:" + this.overdraft);
+    }
+
+}
